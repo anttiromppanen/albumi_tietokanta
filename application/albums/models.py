eec1fbe30b8b385eca53be72fd30d057a2cf4576
Antti Romppanen <romppanen.antti@gmail.com>
2020-03-19 20:40:12 +0200

    osa2 toimii

---
+++ application/albums/models.py
@@ -0,0 +1,12 @@
+from application import db
+
+class Album(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nimi = db.Column(db.String(144), nullable=False)
+    julkaisuvuosi = db.Column(db.Integer, nullable=False)
+    tahtien_maara = db.Column(db.Integer, nullable=False)
+
+    def __init__(self, nimi, julkaisuvuosi, tahtien_maara):
+        self.nimi = nimi
+        self.julkaisuvuosi = julkaisuvuosi
+        self.tahtien_maara = tahtien_maara
---
+++ application/albums/views.py
@@ -0,0 +1,19 @@
+from application import app, db
+from flask import redirect, render_template, request, url_for
+from application.albums.models import Album
+
+@app.route("/albums", methods=["GET"])
+def albums_index():
+    return render_template("albums/list.html", albums = Album.query.all())
+
+@app.route("/albums/new/")
+def albums_form():
+    return render_template("albums/new.html")
+
+@app.route("/albums/", methods=["POST"])
+def albums_create():
+    a = Album(request.form.get("nimi"), request.form.get("julkaisuvuosi"), request.form.get("tahtien_maara"))
+    db.session().add(a)
+    db.session().commit()
+
+    return redirect(url_for("albums_index"))
