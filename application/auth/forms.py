47fb91420824980cddf7b47f43741f64b7518b16
Antti Romppanen <romppanen.antti@gmail.com>
2020-04-04 20:01:24 +0300

    rekisteröitymisen lisäys, albumin lisäyksen ulkoasu uusiksi

--- application/auth/forms.py
+++ application/auth/forms.py
@@ -7,3 +7,10 @@ class LoginForm(FlaskForm):
   
     class Meta:
         csrf = False
+
+class RegisterForm(FlaskForm):
+    username = StringField("Käyttäjänimi", validators=[validators.InputRequired(), validators.Length(min=3)])
+    password = PasswordField("Salasana", validators=[validators.InputRequired(), validators.Length(min=5)])
+
+    class Meta:
+        csrf = False
--- application/auth/views.py
+++ application/auth/views.py
@@ -1,9 +1,9 @@
 from flask import render_template, request, redirect, url_for
 from flask_login import login_user, logout_user
 
-from application import app
+from application import app, db
 from application.auth.models import User
-from application.auth.forms import LoginForm
+from application.auth.forms import LoginForm, RegisterForm
 
 @app.route("/auth/login", methods = ["GET", "POST"])
 def auth_login():
@@ -28,6 +28,22 @@ def auth_logout():
     logout_user()
     return redirect(url_for("index"))
 
-@app.route("/auth/register")
+@app.route("/auth/register", methods = ["GET", "POST"])
 def auth_register():
-    return render_template("auth/registerform.html")
+    if request.method == "GET":
+        return render_template("auth/registerform.html", form = RegisterForm())
+
+    form = RegisterForm(request.form)
+
+    loytyykoUser = User.query.filter_by(username = form.username.data).first()
+    if loytyykoUser:
+        return render_template("auth/registerform.html", form = form, error = "Käyttäjänimi varattu, valitse uusi")
+
+    if not form.validate_on_submit():
+        return render_template("auth/registerform.html", form = form)
+
+    user = User(form.username.data, form.password.data)
+    db.session().add(user)
+    db.session().commit()
+
+    return redirect(url_for("index"))
